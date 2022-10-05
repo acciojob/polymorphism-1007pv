7f28e797e19835ba4e0b2692a929b78bc2899aea
Pankaj Verma <84775453+1007pv@users.noreply.github.com>
2022-10-05 08:50:59 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,27 @@
 package com.driver;
 
 public class Main {
+    class Product {
+        public int product(int x, int y) {
+            System.out.print("x->" + x + " & " + "y->" + y);
+        }
 
-    
-  }
+        public int product(int x, int y, int z) {
+            System.out.print("x->" + x + " & " + "y->" + y + "z->" + z);
+        }
+
+        public double product(double x, double y) {
+            System.out.print("x->" + x + " & " + "y->" + y);
+        }
+
+    }
+
+    public void main(String[] args) {
+        Product p = new Product();
+        p.product(10, 20);
+        p.product(10, 20, 30);
+        p.product(10.123, 20.321);
+
+    }
+
+}
