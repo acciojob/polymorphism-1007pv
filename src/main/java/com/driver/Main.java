a7dbe4771aeafb889d9947180e4133d49d63124e
Pankaj <vpankaj314@gmail.com>
2022-10-08 09:09:20 +0530

    new commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,28 +2,24 @@ package com.driver;
 
 import java.sql.SQLOutput;
 
+class Product {
+    public int product(int x, int y) {
+        return x+y;
+    }
+    public int product(int x, int y, int z) {
+        return x+y+z;
+    }
+    public double product(double x, double y) {
+        return x*y;
+    }
+}
 
 public class Main {
-
-    class Product {
-        public int product(int x, int y) {
-
-            return x+y;
-        }
-        public int product(int x, int y, int z) {
-            return x+y+z;
-        }
-        public double product(double x, double y) {
-            return x*y;
-        }
-    }
-
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Product p = new Product();
         int ans1 = p.product(10, 20);
         int ans2 = p.product(10, 20, 30);
         double ans3 = p.product(10.123, 20.321);
         System.out.println(ans1+" "+ans2+" "+ans3);
     }
-
 }
